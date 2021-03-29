1be3ebaf780f4258fcd8114daa47d41bdad6e455
Mark1708 <m.gurianov@mail.ru>
2021-03-29 09:56:34 +0300

    Using the Eureka nameserver

---
+++ spring-boot-microservice-eureka-naming-server/src/test/java/com/mark1708/springboot/microservice/eureka/naming/server/springbootmicroserviceeurekanamingserver/SpringBootMicroserviceEurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mark1708.springboot.microservice.eureka.naming.server.springbootmicroserviceeurekanamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroserviceEurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
